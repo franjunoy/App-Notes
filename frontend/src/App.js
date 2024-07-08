3cfef6f281848e6e00165097e95fef99e5d29d1f
franjunoy <franjunoy@hotmail.com>
2024-07-08 19:29:51 -0300

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import NoteForm from './component/NoteForm';
 
 const App = () => {
   return (
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center '>
       <h1 className='text-3xl font-bold underline mb-4'>App de notas</h1>
       <NoteForm />
       <NoteList />
